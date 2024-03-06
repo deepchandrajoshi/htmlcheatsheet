3a0e257ba486bfa3088015153f6bddce3c85efbf
Deep Chandra Joshi <itscode.dcj@gmail.com>
2024-03-06 18:00:23 +0530

    add some information

---
+++ Assets/JS/FAQscript.js
@@ -0,0 +1,11 @@
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach((item) => {
+  const question = item.querySelector(".faq-question");
+  const answer = item.querySelector(".faq-answer");
+
+  question.addEventListener("click", () => {
+    // Toggle the 'active' class to show/hide the answer
+    answer.classList.toggle("active");
+  });
+});
